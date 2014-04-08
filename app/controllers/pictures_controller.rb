08f35611665f0bced406e76c7e56803b8794a23e
Sandeep Dhanjani <sandeepdhanjani@gmail.com>
2014-04-08 17:54:57 -0400

    Final

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -23,6 +23,24 @@ class PicturesController < ApplicationController
     end
   end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
 
       # def picture0
       #   @picture = {
@@ -48,13 +66,11 @@ class PicturesController < ApplicationController
       #   }
       # end
 
-  private
-  def picture_params
-    params.require(:picture).permit(:artist, :title, :url)
-  end
-
-
-end
+      private
+      def picture_params
+        params.require(:picture).permit(:artist, :title, :url)
+      end
+ end
 
 
 
