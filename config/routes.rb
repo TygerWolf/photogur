43febd25b0c351a390d1ac70c4d757a22f8781a3
Sandeep Dhanjani <sandeepdhanjani@gmail.com>
2014-04-08 16:37:29 -0400

    Adding feature to create new picutre using HTML form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Photogur::Application.routes.draw do
 
-get 'pictures' => 'pictures#index'
+  get 'pictures' => 'pictures#index'
 
-get 'pictures/:id' => 'pictures#show', as: "picture"
+
+post 'pictures' => "pictures#create" # this is a new line of code
+  get 'pictures/new' => 'pictures#new' # this is also a new line of code
+
+
+  get 'pictures/:id' => 'pictures#show', as: "picture"
 
 
 
